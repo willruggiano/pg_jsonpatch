8039b265776bbe6a07e998584de9f391c0b87aa3
Will Ruggiano <wmruggiano@gmail.com>
2025-04-08 00:47:16 -0700

    feat: <100

--- pg_jsonpatch--1.0.0.sql
+++ pg_jsonpatch--1.0.0.sql
@@ -3,23 +3,20 @@
 create or replace function jsonb_patch_add(target jsonb, path text[], value jsonb)
 returns jsonb
 as $$
-  with x as (
-    select jsonb_typeof(jsonb_extract_path(target, variadic trim_array(path, 1))) as t
-  )
-  select jsonb_set(target, path, value, true)
-  from x
-  where x.t = 'object'
-  union all
-  select jsonb_insert(target, path, value)
-  from x
-  where x.t = 'array' and path[array_length(path, 1)] is distinct from '-'
-  union all
-  select jsonb_insert(target, trim_array(path, 1) || array['-1'], value, true)
-  from x
-  where x.t = 'array' and path[array_length(path, 1)] = '-'
-  limit 1
-$$
-language sql
+declare
+  target_type text := jsonb_typeof(jsonb_extract_path(target, variadic trim_array(path, 1)));
+begin
+  case
+    when target_type = 'object'
+      then return jsonb_set(target, path, value, true);
+    when target_type = 'array' and path[array_length(path, 1)] = '-'
+      then return jsonb_insert(target, trim_array(path, 1) || array['-1'], value, true);
+    when target_type = 'array'
+      then return jsonb_insert(target, path, value);
+    else return null;
+  end case;
+end $$
+language plpgsql
 immutable;
 
 create or replace function jsonb_patch_remove(target jsonb, path text[])
@@ -36,22 +33,13 @@ immutable;
 
 create or replace function jsonb_patch_move(target jsonb, _from text[], path text[])
 returns jsonb
-as $$
-  with old as (
-    select jsonb_extract_path(target, variadic _from) as value
-  )
-
-  select @extschema@.jsonb_patch_add(@extschema@.jsonb_patch_remove(target, _from), path, value)
-  from old
-$$
+as $$ select @extschema@.jsonb_patch_add(@extschema@.jsonb_patch_remove(target, _from), path, jsonb_extract_path(target, variadic _from)) $$
 language sql
 immutable;
 
 create or replace function jsonb_patch_copy(target jsonb, _from text[], path text[])
 returns jsonb
-as $$
-  select @extschema@.jsonb_patch_add(target, path, jsonb_extract_path(target, variadic _from))
-$$
+as $$ select @extschema@.jsonb_patch_add(target, path, jsonb_extract_path(target, variadic _from)) $$
 language sql
 immutable;
 
@@ -67,53 +55,40 @@ as $$ select array_remove(string_to_array(path, '/'), '') $$
 language sql
 immutable;
 
+-- Apply a single patch operation to an object.
 create or replace function jsonb_patch_apply(target jsonb, patch jsonb)
 returns jsonb
 as $$
 declare
-  op text := patch->>'op';
   path text[] := @extschema@.jsonb_patch_split_path(patch->>'path');
-  rv jsonb;
 begin
   case patch->>'op'
-    when 'add' then rv := @extschema@.jsonb_patch_add(target, path, patch->'value');
-    when 'remove' then rv := @extschema@.jsonb_patch_remove(target, path);
-    when 'replace' then rv := @extschema@.jsonb_patch_replace(target, path, patch->'value');
-    when 'move' then rv := @extschema@.jsonb_patch_move(target, @extschema@.jsonb_patch_split_path(patch->>'from'), path);
-    when 'copy' then rv := @extschema@.jsonb_patch_copy(target, @extschema@.jsonb_patch_split_path(patch->>'from'), path);
-    when 'test' then rv := @extschema@.jsonb_patch_test(target, path, patch->'value');
+    when 'add' then return @extschema@.jsonb_patch_add(target, path, patch->'value');
+    when 'remove' then return @extschema@.jsonb_patch_remove(target, path);
+    when 'replace' then return @extschema@.jsonb_patch_replace(target, path, patch->'value');
+    when 'move' then return @extschema@.jsonb_patch_move(target, @extschema@.jsonb_patch_split_path(patch->>'from'), path);
+    when 'copy' then return @extschema@.jsonb_patch_copy(target, @extschema@.jsonb_patch_split_path(patch->>'from'), path);
+    when 'test' then return @extschema@.jsonb_patch_test(target, path, patch->'value');
+    else return null;
   end case;
-
-  return rv;
 end $$
 language plpgsql
 immutable;
 
-create or replace function jsonb_coalesce(variadic jsonb[])
-returns jsonb
-as $$
-  select value
-  from unnest($1) as value
-  where value is not null and jsonb_typeof(value) != 'null'
-  limit 1
-$$
-language sql
-immutable;
-
-create or replace function jsonb_patch_agg(target jsonb, patch jsonb, base jsonb)
-returns jsonb
-as $$ select @extschema@.jsonb_patch_apply(@extschema@.jsonb_coalesce(target, base), patch) $$
-language sql
-immutable;
-
-create or replace aggregate jsonb_patch_agg(jsonb, jsonb) (
-  sfunc = @extschema@.jsonb_patch_agg,
-  stype = jsonb,
-  initcond = null
-);
-
 create or replace function jsonb_patch(target jsonb, patches jsonb)
 returns jsonb
-as $$ select jsonb_patch_agg(value, target) from jsonb_array_elements(patches) $$
-language sql
+as $$
+declare
+  patch jsonb;
+  result jsonb := target;
+begin
+  for patch in select * from jsonb_array_elements(patches) loop
+    result := @extschema@.jsonb_patch_apply(result, patch);
+    if result is null then
+      return null;
+    end if;
+  end loop;
+  return result;
+end $$
+language plpgsql
 immutable;
